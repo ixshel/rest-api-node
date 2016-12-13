07bb63f27182c9c5767b44d22d8427680294fa35
alejandro escamilla <ixshelescamilla@gmail.com>
2016-12-13 11:01:03 -0600

    try it

--- routes/tvshows.js
+++ routes/tvshows.js
@@ -49,9 +49,7 @@ module.exports = function (app) {
                 console.log('ERROR: ' + err);
             }
         });
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    
         res.send(tvshow);
     };
 
--- server.js
+++ server.js
@@ -13,6 +13,9 @@ var router = express.Router();
 app.use(router);
 
 app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.send("Hello world!");
 });
 
