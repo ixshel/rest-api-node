e1d403bea02299b484e62ba93922a657542b6862
alejandro escamilla <ixshelescamilla@gmail.com>
2016-12-13 11:03:11 -0600

    set headers

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ var router = express.Router();
 
 app.use(router);
 
+app.use(function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'Content-Type');
+next();
+});
+
 app.get('/', function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.send("Hello world!");
 });
 
